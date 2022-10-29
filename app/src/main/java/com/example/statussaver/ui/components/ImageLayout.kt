9387e9aea7aaad2976d8ed5a0185d790ac88cd84
Raheem Jnr <mumedian6@gmail.com>
2022-10-29 21:42:51 +0100

    designing UI

---
+++ app/src/main/java/com/example/statussaver/ui/components/ImageLayout.kt
@@ -0,0 +1,15 @@
+package com.example.statussaver.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ImageLayout() {
+    Column(
+        modifier = Modifier
+    ) {
+
+
+    }
+}
