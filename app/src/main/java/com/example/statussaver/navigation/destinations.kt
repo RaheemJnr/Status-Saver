e6444447af199bcc2354c6308fbf238b2cfe24fa
Raheem Jnr <mumedian6@gmail.com>
2022-10-31 15:15:02 +0100

    designing UI

--- app/src/main/java/com/example/statussaver/navigation/destinations.kt
+++ app/src/main/java/com/example/statussaver/navigation/destinations.kt
@@ -8,9 +8,9 @@ sealed class MainScreen(
     val index: Int
 ) {
     object WABusiness :
-        MainScreen(route = "WhatsApp Business", title = "WaBusiness", icon = R.drawable.bottom_bar_local_icon, index = 0)
+        MainScreen(route = "WhatsApp Business", title = "WaBusiness", icon = R.drawable.all_whatsapp_icon, index = 0)
 
-    object Whatsapp : MainScreen(route = "WhatsApp", title = "Whatsapp", icon = R.drawable.bottom_bar_local_icon, index = 1)
+    object Whatsapp : MainScreen(route = "WhatsApp", title = "Whatsapp", icon = R.drawable.all_whatsapp_icon, index = 1)
 
-    object SavedFile : MainScreen(route = "Saved Files", title = "SavedFiles", icon = R.drawable.bottom_bar_online_icon, index = 2)
+    object SavedFile : MainScreen(route = "Saved Files", title = "SavedFiles", icon = R.drawable.saved_file, index = 2)
 }
--- app/src/main/java/com/example/statussaver/ui/components/ImageLayout.kt
+++ app/src/main/java/com/example/statussaver/ui/components/ImageLayout.kt
@@ -1,13 +1,14 @@
 package com.example.statussaver.ui.components
 
+import android.widget.Toast
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Done
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,9 +16,17 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import com.example.statussaver.R
 import com.example.statussaver.model.Status
 import java.io.File
 
@@ -25,62 +34,77 @@ import java.io.File
 fun ImageLayout(
     status: Status
 ) {
+    val context = LocalContext.current
     var editable by rememberSaveable { mutableStateOf(false) }
     Column(
         modifier = Modifier
-//            .size(128.dp)
-//            .wrapContentSize()
-//            .padding(start = 8.dp, top = 4.dp, bottom = 4.dp, end = 4.dp)
-//            .background(Color.Transparent, shape = RoundedCornerShape(6.dp))
-//            .clickable {
-//                editable = !editable
-//            }
-//            .border(1.3.dp, Color.Blue, RoundedCornerShape(8.dp))
-//            .clip(RoundedCornerShape(8.dp))
-//            .clipToBounds()
+            .size(128.dp)
+            .wrapContentSize()
+            .padding(start = 8.dp, top = 4.dp, bottom = 4.dp, end = 4.dp)
+            .background(Color.Transparent, shape = RoundedCornerShape(6.dp))
+            .clickable {
+                editable = !editable
+            }
+            .border(1.3.dp, Color.Blue, RoundedCornerShape(8.dp))
+            .clip(RoundedCornerShape(8.dp))
+            .clipToBounds()
     ) {
         Box(
             modifier = Modifier
-                .fillMaxSize()
-                .size(150.dp),
         ) {
             Image(
                 painter = rememberAsyncImagePainter(
-                    model = Icons.Default.Add,
+                    model = status.file,
                     contentScale = ContentScale.Crop
                 ),
                 contentDescription = "",
                 contentScale = ContentScale.Crop,
                 alignment = Alignment.Center,
             )
+            Box(modifier = Modifier.align(Alignment.BottomCenter)) {
+                androidx.compose.animation.AnimatedVisibility(
+                    visible = editable,
+                    enter = slideInVertically(initialOffsetY = { it }),
+                    exit = slideOutVertically(targetOffsetY = { it }),
+                    content = {
+                        Box(
+                            contentAlignment = Alignment.BottomCenter,
+                            modifier = Modifier.align(Alignment.BottomCenter)
+                        ) {
 
-            Box(
-                modifier = Modifier.align(Alignment.BottomCenter)
-            ) {
-                Image(
-                    imageVector = Icons.Default.Done,
-                    contentDescription = "",
-                    modifier = Modifier.size(220.dp)
+                            Row(
+                                verticalAlignment = Alignment.Bottom,
+                                horizontalArrangement = Arrangement.Center,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(35.dp)
+                                    .alpha(0.4f)
+                                    .background(Color.Black)
+
+
+                            ) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.download_icon),
+                                    contentDescription = "",
+                                    colorFilter = ColorFilter.tint(Color.White),
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .size(30.dp)
+                                        .clickable {
+                                            Toast
+                                                .makeText(context, "clicked", Toast.LENGTH_SHORT)
+                                                .show()
+                                        },
+                                    alpha = 1f,
+                                )
+                            }
+
+                        }
+                    }
                 )
             }
 
 
-/*
-AnimatedVisibility(
-visible = editable
-) {
-
-Row(
-modifier = Modifier.fillMaxWidth(),
-horizontalArrangement = Arrangement.Center
-) {
-
-
-}
-
-
-}
-*/
         }
     }
 
