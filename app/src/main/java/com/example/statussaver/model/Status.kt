2339090f2d999f5a4d9f1cf4c9a03c88daa9aa33
Raheem Jnr <mumedian6@gmail.com>
2022-11-11 15:02:54 +0100

    designing UI(added icon)

--- app/src/main/java/com/example/statussaver/model/Status.kt
+++ app/src/main/java/com/example/statussaver/model/Status.kt
@@ -1,27 +1,16 @@
 package com.example.statussaver.model
 
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
 import java.io.File
 
 const val MP4 = ".mp4"
 
+@Stable
+@Immutable
 data class Status(
     val file: File,
     val title: String,
     val path: String,
-    var isVideo: Boolean = file.name.endsWith(MP4)
-)
-
-val emptyStatus = Status(
-    file = File(""),
-    title = "",
-    path = "",
-    isVideo = false,
-
-    )
-
-data class Statuss(
-    val file: ImageVector,
-    val title: String,
-    var isVideo: Boolean = file.name.endsWith(MP4)
+    val isVideo: Boolean = file.name.endsWith(MP4)
 )
