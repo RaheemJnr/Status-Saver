8dee926ada00bb89aab4a57ea369aa658116f500
Raheem Jnr <mumedian6@gmail.com>
2022-11-27 13:43:23 +0100

    designing UI(added custom tab)

--- app/src/main/java/com/example/statussaver/ui/components/BottomBarUI.kt
+++ app/src/main/java/com/example/statussaver/ui/components/BottomBarUI.kt
@@ -4,9 +4,9 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.BottomAppBar
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
@@ -21,7 +21,7 @@ fun BottomBarUI(
         BottomAppBar(
             modifier = Modifier
                 .height(60.dp),
-            backgroundColor = Color.White,
+            backgroundColor = MaterialTheme.colors.surface,
             elevation = 22.dp,
         ) {
             BottomNav(
--- app/src/main/java/com/example/statussaver/ui/components/BottomNavBar.kt
+++ app/src/main/java/com/example/statussaver/ui/components/BottomNavBar.kt
@@ -8,16 +8,12 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -40,62 +36,66 @@ private fun HomeBottomItem(
 ) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
-    BottomNavigation(
-        modifier = Modifier
-            .navigationBarsPadding()
-            .padding(10.dp, 0.dp, 10.dp, 0.dp)
-            .height(100.dp),
-        backgroundColor = Color.White,
-        elevation = 0.dp
+    Surface(
+        color = MaterialTheme.colors.surface
     ) {
-        items.forEach {
-            BottomNavigationItem(
-                alwaysShowLabel = true,
-                modifier = Modifier,
-                icon = {
-                    Icon(
-                        painter = painterResource(id = it.icon),
-                        contentDescription = "",
-                        modifier = Modifier
-                            .width(dimension[it.index].dp)
-                            .height(dimension[it.index].dp)
-                            .animateContentSize(),
-                    )
-                },
-                label = {
-                    Text(
-                        text = it.title,
-                        color = Color.LightGray
-                    )
-                },
-                //
-                selected = currentRoute == it.route,
-                onClick = {
-                    it.route.let { destination ->
-                        navController.navigate(destination) {
-                            // Pop up to the start destination of the graph to
-                            // avoid building up a large stack of destinations
-                            // on the back stack as users select items
-                            popUpTo(navController.graph.findStartDestination().id) {
-                                saveState = true
+        BottomNavigation(
+            modifier = Modifier
+                .navigationBarsPadding()
+                .padding(10.dp, 0.dp, 10.dp, 0.dp)
+                .height(100.dp),
+            backgroundColor = MaterialTheme.colors.background,
+            elevation = 0.dp
+        ) {
+            items.forEach {
+                BottomNavigationItem(
+                    alwaysShowLabel = true,
+                    modifier = Modifier,
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = it.icon),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .width(dimension[it.index].dp)
+                                .height(dimension[it.index].dp)
+                                .animateContentSize(),
+                        )
+                    },
+                    label = {
+                        Text(
+                            text = it.title,
+                            color = MaterialTheme.colors.onBackground
+                        )
+                    },
+                    //
+                    selected = currentRoute == it.route,
+                    onClick = {
+                        it.route.let { destination ->
+                            navController.navigate(destination) {
+                                // Pop up to the start destination of the graph to
+                                // avoid building up a large stack of destinations
+                                // on the back stack as users select items
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                // Avoid multiple copies of the same destination when
+                                // reSelecting the same item
+                                launchSingleTop = true
+                                // Restore state when reSelecting a previously selected item
+                                restoreState = true
                             }
-                            // Avoid multiple copies of the same destination when
-                            // reSelecting the same item
-                            launchSingleTop = true
-                            // Restore state when reSelecting a previously selected item
-                            restoreState = true
                         }
-                    }
-                    dimension.forEachIndexed { index, _ ->
-                        if (index == it.index)
-                            dimension[index] = 36
-                        else
-                            dimension[index] = 35
-                    }
-                },
-                selectedContentColor = Color.Black,
-                unselectedContentColor = Color.Green,
-            )
+                        dimension.forEachIndexed { index, _ ->
+                            if (index == it.index)
+                                dimension[index] = 36
+                            else
+                                dimension[index] = 35
+                        }
+                    },
+                    selectedContentColor = MaterialTheme.colors.primary,
+                    unselectedContentColor =MaterialTheme.colors.onBackground,
+                )
+            }
         }
     }
 }
